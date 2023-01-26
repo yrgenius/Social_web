f619ac9386d7d550044f2160947274dc9995fdb3
yrgenius <yrgenius80@gmail.com>
2023-01-26 21:13:22 +0900

    lesson 10

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return(
+        <div>
+            <ul>
+                <a href="#">Home</a>
+                <a href="#">News</a>
+                <a href="#">Message</a>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
