3644f3e11ae20d8ab881066adddd41635e7a2dba
yrgenius <yrgenius80@gmail.com>
2023-02-01 20:48:21 +0900

    lesson 20

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Navbar />
-      <div className='wrapper'>
-        {/* <Profile /> */}
-        <Dialogs />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Navbar />
+        <div className='wrapper'>
+          <Routes>
+            <Route element={<Profile />} path='/profile' />
+            <Route element={<Dialogs />} path='/dialogs' />
+          </Routes>
+        </div>
       </div>
-
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <nav className={styles.AppNav}>
       <ul className={`${styles.list} ${styles.ul}`}>
-        <li><a href='#'>Messages</a></li>
-        <li><a href='#'>Profile</a></li>
+        <li><a href='/profile'>Profile</a></li>
+        <li><a href='/dialogs'>Messages</a></li>
         <li><a href='#'>News</a></li>
         <li><a href='#'>Music</a></li>
         <li><a href='#'>Settings</a></li>
